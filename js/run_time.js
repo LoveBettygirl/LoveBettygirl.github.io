dd350af1b68fd2e186e628f4fba1627bc2de5388
LoveBettygirl <bettycyygirl@163.com>
2021-03-27 09:30:06 +0800

    Site updated: 2021-03-27 09:30:05

--- js/run_time.js
+++ js/run_time.js
@@ -3,20 +3,20 @@ var now = new Date();
 function createtime() {
     var grt = new Date('07/31/2019 17:43:07'); //此处修改你的建站时间或者网站上线时间
     now.setTime(now.getTime() + 250);
-    days = (now - grt) / 1000 / 60 / 60 / 24;
-    dnum = Math.floor(days);
-    hours = ((now - grt) / 1000 / 60 / 60 - (24 * dnum)) % 60;
-    hnum = Math.floor(hours);
+    var days = (now - grt) / 1000 / 60 / 60 / 24;
+    var dnum = Math.floor(days);
+    var hours = ((now - grt) / 1000 / 60 / 60 - (24 * dnum)) % 60;
+    var hnum = Math.floor(hours);
     if (String(hnum).length == 1) {
         hnum = '0' + hnum;
     }
-    minutes = ((now - grt) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum)) % 60;
-    mnum = Math.floor(minutes);
+    var minutes = ((now - grt) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum)) % 60;
+    var mnum = Math.floor(minutes);
     if (String(mnum).length == 1) {
         mnum = '0' + mnum;
     }
-    seconds = ((now - grt) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum)) % 60;
-    snum = Math.round(seconds);
+    var seconds = ((now - grt) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum)) % 60;
+    var snum = Math.round(seconds);
     if (String(snum).length == 1) {
         snum = '0' + snum;
     }
